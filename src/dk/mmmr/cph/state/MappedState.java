d1c28d26bc724ea97029975bd2b481ce3e9ad3eb
CONVERGENS\mak <mak@convergens.lan>
2021-04-08 16:18:05 +0200

    first

---
+++ src/dk/mmmr/cph/state/MappedState.java
@@ -0,0 +1,39 @@
+package dk.mmmr.cph.state;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MappedState implements State {
+    private Map<String, ValueSet> variables = new HashMap<>();
+
+    public MappedState() {
+    }
+
+    public void put(String variableName, ValueSet values) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void put(String variableName) {
+        // all values
+    }
+
+    @Override
+    public ValueSet valuesOf(String variableName) {
+        return null;
+    }
+
+    @Override
+    public boolean subStateOf(State other) {
+        if (other instanceof MappedState) {
+            var mapped = (MappedState) other;
+            for (String variableName : mapped.variables.keySet()) {
+                var thisVariable = variables.get(variableName);
+                if (thisVariable == null) return false;
+                var otherVariable = mapped.variables.get(variableName);
+                return thisVariable.subsetOf(otherVariable);
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/dk/mmmr/cph/state/VSSL.java
@@ -0,0 +1,20 @@
+package dk.mmmr.cph.state;
+
+public class VSSL {
+    public static void main(String[] args) {
+
+        //TODO this
+/*
+        State P = new XState(...); // precondition
+        State Q = new XState(...); // postcondition
+        SyntaxtTree C = new SyntaxTree(...) // commands / code / program
+        State S = C.analyse(P);
+        if (S.subStateOf(Q) {
+            System.out.println("Hurra!");
+        }
+    else{
+            System.out.println("Øv!");
+        }
+*/
+    }
+}
